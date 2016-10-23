d412926d5e1d31afb4134f618bbe9e4fbb6c14d3
yituo <823808777@qq.com>
2016-10-23 11:14:46 +0800

    first aja

---
+++ aja.js
@@ -0,0 +1,31 @@
+function ajax(ajson){
+	var xhr=window.XMLHttpRequest?new XMLHttpRequest():new ActiveXObject('Microsoft.XMLHTTP');	
+	var method=ajson.method||'get',
+		url=ajson.url,
+		ayan=ajson.ayan||true,
+		data=ajson.data||'',
+		success=ajson.success,
+		error=ajson.error;
+		if(method.toLowerCase()=='get'){
+			url+='?'+data+new Date().getTime();	
+		}
+		xhr.open(method ,url,ayan);
+		xhr.setRequestHeader('content-type' , 'application/x-www-form-urlencoded');
+		xhr.send(data);
+		xhr.onreadystatechange=function(){
+			if(xhr.readState==4){
+				success&&success( xhr.responseText );
+			}else{
+				error&&error();
+			}
+		}
+}
+// ajax调 用 内 容 格 式。。。
+/*ajax({
+	'method':'post',
+	'url':'abc.php',
+	'ayan':true,
+	'data':'123456',
+	'success':function(){},
+	'error':function(){}
+	});*/
